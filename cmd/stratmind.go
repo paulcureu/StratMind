fb29cb55ffb60367383789e7a6b78d94dc662ab6
Paul <cureu.paul.ioan@gmail.com>
2025-04-14 02:17:26 +0300

    Implement player path tracking and zone mapping

--- cmd/stratmind.go
+++ cmd/stratmind.go
@@ -1,1 +1,11 @@
 package main
+
+import "stratmind/core/parser"
+
+func main() {
+	demoPath := "demos/falcons-vs-faze-m3-mirage.dem"
+	outputPath := "D:\\VSCode\\StratMind\\core\\export\\timelines.json"
+
+	parser.TrackRounds(demoPath, outputPath)
+}
+
