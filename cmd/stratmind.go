510b4b487e1f70b73aa06af1fd85af4bc4a8f4d8
Paul <cureu.paul.ioan@gmail.com>
2025-04-14 15:25:58 +0300

    update

--- cmd/stratmind.go
+++ cmd/stratmind.go
@@ -1,11 +1,23 @@
 package main
 
-import "stratmind/core/parser"
+import (
+	"fmt"
+	"log"
+
+	// Import your local parser code using module name + folder path
+	"stratmind/core/parser"
+)
 
 func main() {
-	demoPath := "demos/falcons-vs-faze-m3-mirage.dem"
-	outputPath := "D:\\VSCode\\StratMind\\core\\export\\timelines.json"
+    // CHANGE THIS to your real .dem file
+    demoPath := "D:\\VSCode\\StratMind\\demos\\falcons-vs-faze-m3-mirage.dem"
+    outputPath := "core/export/timelines.json"
 
-	parser.TrackRounds(demoPath, outputPath)
+    fmt.Println("Starting parser on:", demoPath)
+    err := parser.TrackRounds(demoPath, outputPath)
+    if err != nil {
+        log.Fatalf("Error parsing demo: %v", err)
+    }
+
+    fmt.Println("Done! JSON saved to:", outputPath)
 }
-
